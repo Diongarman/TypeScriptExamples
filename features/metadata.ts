1251adc28af57268463ea05179723c9f46af6559
Dion Garman <diongarman@Dions-MacBook-Pro.local>
2020-07-22 12:20:33 +0100

    decorated class and stored and retrieved metadata

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 
-//@controller
+@controller
 class Plane {
   color: string = "red";
 
@@ -10,15 +10,18 @@ class Plane {
   }
 }
 
-// function controller(target: Plane){
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata("path", target.prototype, key);
+    console.log(path);
+  }
+}
 
-// }
-
-function get(metadataKey: string) {
+function get(secretInfo: string) {
   return function (target: Plane, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata(metadataKey, "some secret", target, key);
+    Reflect.defineMetadata("path", secretInfo, target, key);
   };
 }
 
-const secret = Reflect.getMetadata("/login", Plane.prototype, "fly");
-console.log(secret);
+// const secret = Reflect.getMetadata("path", Plane.prototype, "fly");
+// console.log(secret);
